dd1bdeff9fb8441ec9d68adf8eaaa6e87900ad1e
SleepRain <1819503703@qq.com>
2023-08-12 12:01:54 +0800

    生成验证码放入缓存

--- src/main/java/com/hnsw/SmsSpringApplication.java
+++ src/main/java/com/hnsw/SmsSpringApplication.java
@@ -2,8 +2,11 @@ package com.hnsw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+//开启缓存功能
+@EnableCaching
 public class SmsSpringApplication {
 
     public static void main(String[] args) {
